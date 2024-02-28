0ac2810e6665d92fa8e51c253f1d148707df0b8f
Ethan W. <160505433+etw07@users.noreply.github.com>
2024-02-28 01:55:48 +0000

    final submission

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ seconds = input('Enter the number of seconds (integer): ')
 seconds = int(seconds)
 
 # ... complete the code below
-
-
-
+hours = seconds//3600
+minutes = (seconds - 3600*hours)//60
+seconds = seconds - minutes*60 - hours*3600
 # Follow the formatting given
 # e.g. The duration is X hours, X minutes, and X seconds.
 print("The duration is", hours, "hours,", minutes, "minutes, and", seconds, "seconds.")
